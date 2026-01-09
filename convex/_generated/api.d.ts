55f51203ec7ce7df61d1d5a80473e15f0d06c2f4
ahmedabelrahman-dev <rman79340@gmail.com>
2026-01-09 22:55:25 +0400

    post and notification mutation and query

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -9,6 +9,8 @@
  */
 
 import type * as http from "../http.js";
+import type * as notifications from "../notifications.js";
+import type * as posts from "../posts.js";
 import type * as users from "../users.js";
 
 import type {
@@ -19,6 +21,8 @@ import type {
 
 declare const fullApi: ApiFromModules<{
   http: typeof http;
+  notifications: typeof notifications;
+  posts: typeof posts;
   users: typeof users;
 }>;
 
