8ffbcfbfacf629b8a5c9324669a980b240b6ba7d
ahmedabelrahman-dev <rman79340@gmail.com>
2026-01-10 21:55:05 +0400

    type as bookmark, comment , post

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,8 @@
  * @module
  */
 
+import type * as bookmarks from "../bookmarks.js";
+import type * as comments from "../comments.js";
 import type * as http from "../http.js";
 import type * as notifications from "../notifications.js";
 import type * as posts from "../posts.js";
@@ -20,6 +22,8 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  bookmarks: typeof bookmarks;
+  comments: typeof comments;
   http: typeof http;
   notifications: typeof notifications;
   posts: typeof posts;
