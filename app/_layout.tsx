69d6c567baba90c3854a0a2f86dd493da4a1b05f
ahmedabelrahman-dev <rman79340@gmail.com>
2026-01-11 00:32:11 +0400

    navigator in android platform fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,11 @@
 import InitialLayout from '@/components/InitialLayout';
 import ClerkAndConvexProvider from '@/providers/ClerkAndConvexProvider';
 import { useFonts } from 'expo-font';
+import * as NavigationBar from 'expo-navigation-bar';
 import { SplashScreen } from 'expo-router';
-import { useCallback } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { useCallback, useEffect } from 'react';
+import { Platform } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 SplashScreen.preventAutoHideAsync();
@@ -16,7 +19,13 @@ export default function RootLayout() {
     if (fontsLoaded) await SplashScreen.hideAsync();
   }, [fontsLoaded]);
 
-  if (!fontsLoaded) return null;
+  // update the native navigation bar on Android.
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      NavigationBar.setBackgroundColorAsync('#000000');
+      NavigationBar.setButtonStyleAsync('light');
+    }
+  }, []);
   return (
     <ClerkAndConvexProvider>
       <SafeAreaProvider>
@@ -24,6 +33,7 @@ export default function RootLayout() {
           <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
+      <StatusBar backgroundColor="transparent" translucent style="dark" />
     </ClerkAndConvexProvider>
   );
 }
