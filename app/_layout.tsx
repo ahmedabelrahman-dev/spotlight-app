11f7ac45e5e8ae63ede922872030a4282b2e15cc
ahmedabelrahman-dev <rman79340@gmail.com>
2026-01-10 21:51:51 +0400

    font for the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,26 @@
 import InitialLayout from '@/components/InitialLayout';
 import ClerkAndConvexProvider from '@/providers/ClerkAndConvexProvider';
+import { useFonts } from 'expo-font';
+import { SplashScreen } from 'expo-router';
+import { useCallback } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'JetBrainsMono-Medium': require('../assets/fonts/JetBrainsMono-Medium.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
   return (
     <ClerkAndConvexProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
           <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
